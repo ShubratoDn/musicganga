029046babda44e4f15ab65e12dab41fd42cd8a9f
My Computer <mycomputer44985@gmail.com>
2021-10-03 22:55:14 +0600

    111

---
+++ js/musicGanga.js
@@ -0,0 +1,15 @@
+// scrolling script start
+var navbar = document.querySelector(".nav-scroll");
+
+document.addEventListener('scroll', function(){
+  var scroll_position = window.scrollY;
+  if (scroll_position > 200) {
+    navbar.style.position = 'fixed';
+  } else {
+    navbar.style.position = 'initial';
+  }
+});
+//   ends
+
+// Aos
+AOS.init();
